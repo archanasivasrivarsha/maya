5212c31be402e0d7716579b15d2c04f1ccb9c80b
archanasivasrivarsha <charchana2007@gmail.com>
2025-12-05 22:50:56 +0530

    Add files via upload

---
+++ surface area and volume of a cube.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int side;
+    scanf("%d", &side);
+
+    // Calculate surface area and volume
+    int surface_area = 6 * side * side;
+    int volume = side * side * side;
+
+    // Output the results
+    printf("Surface area = %d and Volume = %d
+", surface_area, volume);
+
+    return 0;
+}

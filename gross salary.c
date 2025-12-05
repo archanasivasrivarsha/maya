00c0a4aa96eb3c860f8242c42a34bf33cc5d71a7
archanasivasrivarsha <charchana2007@gmail.com>
2025-12-05 23:11:58 +0530

    Update gross salary.c

--- gross salary.c
+++ gross salary.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int basic_salary;
     float da, hra, gross_salary;
 
@@ -8,13 +9,18 @@ int main() {
     scanf("%d", &basic_salary);
 
     // Conditions
-    if (basic_salary <= 10000) {
+    if (basic_salary <= 10000)
+    {
         da = 0.80 * basic_salary;
         hra = 0.20 * basic_salary;
-    } else if (basic_salary <= 20000) {
+    }
+    else if (basic_salary <= 20000)
+    {
         da = 0.90 * basic_salary;
         hra = 0.25 * basic_salary;
-    } else {
+    }
+    else
+    {
         da = 0.95 * basic_salary;
         hra = 0.30 * basic_salary;
     }
@@ -23,3 +29,4 @@ int main() {
     gross_salary = basic_salary + da + hra;
 
     // Output with 2 decimal places
+

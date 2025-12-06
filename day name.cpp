3ecca2dcbe9eea5af359167a50990927fa604a5a
archanasivasrivarsha <charchana2007@gmail.com>
2025-12-06 20:46:46 +0530

    Add files via upload

---
+++ day name.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+ {
+    int day;
+    scanf("%d", &day);
+
+    if (day == 1).
+     {
+        printf("Monday");
+    }
+    else if (day == 2)
+     {
+        printf("Tuesday");
+    }
+    else if (day == 3)
+     {
+        printf("Wednesday");
+    }
+    else if (day == 4) 
+    {
+        printf("Thursday");
+    }
+    else if (day == 5)
+     {
